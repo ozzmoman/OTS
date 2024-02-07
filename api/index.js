7e80ebef0ae336efb7bf5f68ef079e343bbfb32a
ozzmoman <oswald.cobolpot@gmail.com>
2024-02-07 15:28:33 +0330

    sec commite

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import userRouter from './routes/user.route.js';
 
 mongoose.connect(process.env.MONGODB).then(() => {
     console.log('Connect to MongoDB!');
@@ -16,6 +17,13 @@ app.listen(4000, () => {
     console.log('Server run in 4000..');
 })
 
-app.get("/api", (req, res) => {
+app.use("/api/user", userRouter);
+
+app.post("/api/register", (req, res) => {
+    
     res.json({ message: "Hello world" });
+
+//    const data = req.body;
+  //  console.log({ email, password, tel, username });
 });
+
